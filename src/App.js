2d8a9eba466fb1255209342e69504a9e90cda552
Nhu Kangasniemi <nhu.kangasniemi@symbio.com>
2020-09-10 18:00:37 +0300

    Docker file set up for Development and Production

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello Nhu. It's a new day!
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn To Love and Peace
         </a>
       </header>
     </div>
